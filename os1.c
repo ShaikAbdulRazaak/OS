5f9df8a6b8e7b73e4ca723d73af760e0404c2258
Shaik Abdul Razaak <abdulrazaaks404@gmail.com>
2019-04-10 15:46:14 +0530

    Second Commit

--- os1.c
+++ os1.c
@@ -4,18 +4,23 @@
 #include<stdlib.h>
 #include<semaphore.h>
 void *fun();
-int i,n;
-const int x=(rand()%n);
+int i,n,x;
+
 int main()
 {
  pthread_t t[10000];
+ printf("\nEnter number of tickets");
  scanf("%d",&n);
+ if(n<=0)
+	{
+		printf("\n\n::::  Invalid Value Of Number Of Tickets  ::::\n");
+		exit(0);
+	}
  for(i=1;i<=n;i++)
  {
  	pthread_create(&t[i],NULL,&fun,NULL);
  	pthread_join(t[i],NULL);
  }
-	
  	printf("The ticket number generated randomly is %d\n",x);
 	printf("%d\n",x);
 }
